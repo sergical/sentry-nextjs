fa5917c035979799274dbf592dcbadb349cc7326
Sergiy Dybskiy <sergiy.dybskiy@sentry.io>
2025-10-30 16:32:22 -0400

    proxy

--- middleware.ts
+++
@@ -1,7 +0,0 @@
-import * as Sentry from "@sentry/nextjs";
-import { type NextRequest, NextResponse } from "next/server";
-
-export default function middleware(request: NextRequest) {
-  Sentry.logger.info("Proxy request", { requestUrl: request.url });
-  return NextResponse.next();
-}
