653ed05b002e3a3c33a00e8b693a0804695823c9
Sergiy Dybskiy <sergiy.dybskiy@sentry.io>
2025-10-30 14:47:53 -0400

    middleware instead of proxy

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 import { type NextRequest, NextResponse } from "next/server";
 
-export default function proxy(request: NextRequest) {
+export default function middleware(request: NextRequest) {
   Sentry.logger.info("Proxy request", { requestUrl: request.url });
   return NextResponse.next();
 }
